3bc33b49e48f8e0a46b90bc26f292e4b59dc4d68
AJ852000 <ajay.jadhav@mindbowser.com>
2025-09-19 11:57:46 +0530

    test

--- lib/modules/flow_test/bloc/flowTestState.dart
+++ lib/modules/flow_test/bloc/flowTestState.dart
@@ -3,7 +3,6 @@ part of 'flowTestBloc.dart';
 @immutable
 sealed class FlowTestState {}
 
-// this bloc uses for managing states related to flow test data fetching and handling.
 final class FlowTestInitial extends FlowTestState {}
 
 final class FlowTestLoading extends FlowTestState {}
@@ -17,5 +16,3 @@ final class FlowTestError extends FlowTestState {
   final String message;
   FlowTestError(this.message);
 }
-
-// this bloc uses for managing states related to flow test data fetching and handling.
